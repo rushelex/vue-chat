ed7175f1e8c2e53d5a36dd141cbe75dd9363bf5b
Алексей Шелементьев <rushelex@gmail.com>
2020-03-02 13:51:55 +0500

    вывод системных сообщений

--- server/app.js
+++ server/app.js
@@ -3,8 +3,23 @@ const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
+const m = (name, text, id) => ({ name, text, id })
+
 io.on('connection', (socket) => {
-  console.log('IO Connected')
+  socket.on('userJoined', (data, cb) => {
+    if (!data.name || !data.room) {
+      // eslint-disable-next-line standard/no-callback-literal
+      return cb('Данные некорректны!')
+    }
+
+    socket.join(data.room)
+    // eslint-disable-next-line standard/no-callback-literal
+    cb({ userId: socket.id })
+    socket.emit('newMessage', m('admin', `Добро пожаловать, ${data.name}!`))
+    socket.broadcast
+      .to(data.room)
+      .emit('newMessage', m('admin', `Пользователь ${data.name} зашёл.`))
+  })
 
   socket.on('createMessage', (data) => {
     setTimeout(() => {
